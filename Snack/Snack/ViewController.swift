cc7996d9c224f1d4c45b9015eb68a5c012d2eb85
chiranjitsarkar92@gmail.com <qratilabs@gmail.com>
2019-11-22 00:28:30 +0530

    Starting Project

---
+++ Snack/Snack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snack
+//
+//  Created by Qrati Labs on 11/22/19.
+//  Copyright © 2019 Qrati Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
